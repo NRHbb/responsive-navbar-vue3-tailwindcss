e18f4936d1db47fff70d9bc82f83785b08fb0895
NR Habibi <nrhbb14@gmail.com>
2023-12-04 15:38:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+<NavBar/>
+
+<h1 class="text-3xl font-extrabold text-center h-screen w-full flex justify-center items-center">Responsive Navbar with Tailwind CSS in Vue3 composition API</h1>
+</template>
+
+<style scoped>
+
+</style>
