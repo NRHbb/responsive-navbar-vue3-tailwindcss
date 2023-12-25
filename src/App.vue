5d573970e0d0120d2295681265ecbff9b34bc236
NR Habibi <nrhbb14@gmail.com>
2023-12-25 11:19:36 +0800

    add one more page to try the transition navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import Page2 from './components/Page2.vue';
 </script>
 
 <template>
-<NavBar/>
+    <div class="">
+        <NavBar />
 
-<h1 class="text-3xl font-extrabold text-center h-screen w-full flex justify-center items-center">Responsive Navbar with Tailwind CSS in Vue3 composition API</h1>
+        <h1 class="text-3xl font-extrabold text-center h-screen w-full flex justify-center items-center bg-orange-500 ">Responsive Navbar
+            with Tailwind CSS in Vue3 composition API</h1>
+
+        <Page2 />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Page2.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-screen w-full flex justify-center items-center flex-col">
+        <h1 class="text-3xl font-bold">Page 2</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
